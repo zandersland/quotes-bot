9aa354c9d25d44dff15cb72561edfc31872cf83c
zandersland <zandersland21@gmail.com>
2022-08-10 12:14:16 -0500

    Base code for a basic bot

---
+++ bot.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+import discord
+import asyncio
+import random
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='q!')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py') and not filename.startswith('_'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+bot.run(TOKEN)
---
+++ cogs/basic.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+import discord
+
+class Basic(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('Pong!')
+
+def setup(bot):
+    bot.add_cog(Basic(bot))
